2f851c9015941c25f65b1aee93347f43ed245e11
saadaoui-salah <salahsaadaoui8@gmail.com>
2026-01-26 06:22:55 +0100

    update

--- core/spiders/q_straight.py
+++ core/spiders/q_straight.py
@@ -160,64 +160,60 @@ class RacingqueenslandSpider(scrapy.Spider):
                 for table in tables:
                     # Convert each row into a Scrapy item-like dict
                     for i, row in enumerate(table, start=1):
-                        try:
-                            item =  {
-                                "Race_time": self.format_time(text),
-                                "Race_Title": text[1].split('-')[1].strip(),
-                                "distance": '300M',
-                                "Race_number": race_number,
-                                "Date": text[2].split(' - ')[0].strip(),
-                                "State": text[0].split(',')[1].strip(),
-                                "Track": ''.join(text[0].split(',')[0].split(' ')[1:]).strip(),
-                                "Race_ID": f'{race[0]}_{race[1]}_{race_number}',
-                                "Dog_Name": row[1] if row[1].lower() != 'race' else '',
-                                "Box": row[2],
-                                "Placing": i,
-                                "Finish_time": row[-1].split('[')[0],
-                                "Top_speed_KMH": row[3].split('KM')[0],
-                                
-                                "50m_time": row[4].split('\n')[0].split('[')[0],
-                                "50m_speed": row[4].split('\n')[1].replace('KM/H',''),
-                                "section_50_meters_to_rail_for_section": self.get_to_rail(row[4]),
-                                "sec_50_rank":row[4].split('\n')[0].split('[')[1].split(']')[0],
-                                
-                                "100m_time": row[5].split('\n')[0].split('[')[0],
-                                "100m_speed": row[5].split('\n')[1].replace('KM/H',''),
-                                "section_100_meters_to_rail_for_section": self.get_to_rail(row[5]),
-                                "sec_100_rank":row[5].split('\n')[0].split('[')[1].split(']')[0],
-                                
-                                "150m_time": row[6].split('\n')[0].split('[')[0],
-                                "150m_speed": row[6].split('\n')[1].replace('KM/H',''),
-                                "section_150_meters_to_rail_for_section": self.get_to_rail(row[6]),
-                                "sec_150_rank":row[6].split('\n')[0].split('[')[1].split(']')[0],
-                                
-                                "200m_time": row[7].split('\n')[0].split('[')[0],
-                                "200m_speed": row[7].split('\n')[1].replace('KM/H',''),
-                                "section_200_meters_to_rail_for_section": self.get_to_rail(row[7]),
-                                "sec_200_rank":row[7].split('\n')[0].split('[')[1].split(']')[0],
-                                
-                                "250m_time": row[8].split('\n')[0].split('[')[0],
-                                "250m_speed": row[8].split('\n')[1].replace('KM/H',''),
-                                "section_250_meters_to_rail_for_section": self.get_to_rail(row[8]),
-                                "sec_250_rank":row[8].split('\n')[0].split('[')[1].split(']')[0],
-                                
-                                "300m_time": row[9].split('\n')[0].split('[')[0],
-                                "300m_speed": row[9].split('\n')[1].replace('KM/H',''),
-                                "section_300_meters_to_rail_for_section": self.get_to_rail(row[9]),
-                                "sec_300_rank":row[9].split('\n')[0].split('[')[1].split(']')[0],
-                                
-                                "Run_home": row[10].split('\n')[0].split('[')[0],
-                                "Hme_speed": row[10].split('\n')[1].replace('KM/H',''),
-                            }
-                            if len(row) == 13:
-                                item["distance"] = "350M"
-                                item["350m_time"] = row[10].split('\n')[0].split('[')[0]
-                                item["350m_speed"] = row[10].split('\n')[1].replace('KM/H','')
-                                item["section_350_meters_to_rail_for_section"] = self.get_to_rail(row[10])
-                                item["sec_300_rank"] = row[10].split('\n')[0].split('[')[1].split(']')[0]
-                                item["Run_home"] = row[11].split('\n')[0].split('[')[0]
-                                item["Hme_speed"] = row[11].split('\n')[1].replace('KM/H','')
-                            print(item)
-                            yield item
-                        except: 
-                            breakpoint()
+                        item =  {
+                            "Race_time": self.format_time(text),
+                            "Race_Title": text[1].split('-')[1].strip(),
+                            "distance": '300M',
+                            "Race_number": race_number,
+                            "Date": text[2].split(' - ')[0].strip(),
+                            "State": text[0].split(',')[1].strip(),
+                            "Track": ''.join(text[0].split(',')[0].split(' ')[1:]).strip(),
+                            "Race_ID": f'{race[0]}_{race[1]}_{race_number}',
+                            "Dog_Name": row[1] if row[1].lower() != 'race' else '',
+                            "Box": row[2],
+                            "Placing": i,
+                            "Finish_time": row[-1].split('[')[0],
+                            "Top_speed_KMH": row[3].split('KM')[0],
+                            
+                            "50m_time": row[4].split('\n')[0].split('[')[0],
+                            "50m_speed": row[4].split('\n')[1].replace('KM/H',''),
+                            "section_50_meters_to_rail_for_section": self.get_to_rail(row[4]),
+                            "sec_50_rank":row[4].split('\n')[0].split('[')[1].split(']')[0],
+                            
+                            "100m_time": row[5].split('\n')[0].split('[')[0],
+                            "100m_speed": row[5].split('\n')[1].replace('KM/H',''),
+                            "section_100_meters_to_rail_for_section": self.get_to_rail(row[5]),
+                            "sec_100_rank":row[5].split('\n')[0].split('[')[1].split(']')[0],
+                            
+                            "150m_time": row[6].split('\n')[0].split('[')[0],
+                            "150m_speed": row[6].split('\n')[1].replace('KM/H',''),
+                            "section_150_meters_to_rail_for_section": self.get_to_rail(row[6]),
+                            "sec_150_rank":row[6].split('\n')[0].split('[')[1].split(']')[0],
+                            
+                            "200m_time": row[7].split('\n')[0].split('[')[0],
+                            "200m_speed": row[7].split('\n')[1].replace('KM/H',''),
+                            "section_200_meters_to_rail_for_section": self.get_to_rail(row[7]),
+                            "sec_200_rank":row[7].split('\n')[0].split('[')[1].split(']')[0],
+                            
+                            "250m_time": row[8].split('\n')[0].split('[')[0],
+                            "250m_speed": row[8].split('\n')[1].replace('KM/H',''),
+                            "section_250_meters_to_rail_for_section": self.get_to_rail(row[8]),
+                            "sec_250_rank":row[8].split('\n')[0].split('[')[1].split(']')[0],
+                            
+                            "300m_time": row[9].split('\n')[0].split('[')[0],
+                            "300m_speed": row[9].split('\n')[1].replace('KM/H',''),
+                            "section_300_meters_to_rail_for_section": self.get_to_rail(row[9]),
+                            "sec_300_rank":row[9].split('\n')[0].split('[')[1].split(']')[0],
+                            
+                            "Run_home": row[10].split('\n')[0].split('[')[0],
+                            "Hme_speed": row[10].split('\n')[1].replace('KM/H',''),
+                        }
+                        if len(row) == 13:
+                            item["distance"] = "350M"
+                            item["350m_time"] = row[10].split('\n')[0].split('[')[0]
+                            item["350m_speed"] = row[10].split('\n')[1].replace('KM/H','')
+                            item["section_350_meters_to_rail_for_section"] = self.get_to_rail(row[10])
+                            item["sec_300_rank"] = row[10].split('\n')[0].split('[')[1].split(']')[0]
+                            item["Run_home"] = row[11].split('\n')[0].split('[')[0]
+                            item["Hme_speed"] = row[11].split('\n')[1].replace('KM/H','')
+                        yield item

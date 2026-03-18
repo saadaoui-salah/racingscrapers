14ed0e2a5f81ffdb5bf7d65311bed137b0be3b2a
saadaoui-salah <salahsaadaoui8@gmail.com>
2026-03-18 03:33:27 +0100

    update

--- core/spiders/ipswich_csv.py
+++
@@ -1,8 +0,0 @@
-from core.spiders.q_straight_csv import RacingqueenslandCSVSpider
-
-
-class IpswichCSVSpider(RacingqueenslandCSVSpider):
-    name = 'ipswich_csv'
-    race_name = "ipswich"
-    csv_url = "https://www.racingqueensland.com.au/industry/harness/harness-sectionals"
-    s3_prefix = "/csv/qld-racing/races/ipswich"
